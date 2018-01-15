b76206ce0b7c3158205224f5c2cd95ef906edcca
zhuangAnjun <547755010@qq.com>
2018-01-15 15:47:28 +0800

    格式调整

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-#对variable创建变量进行测试
+# 对variable创建变量进行测试
 
 # 载入模块
 import numpy as np
@@ -8,38 +8,38 @@ sess = tf.Session()
 
 ############test 1##################
 
-#创建w变量
+# 创建w变量
 w = tf.Variable(3)
-#创建assign_op操作
+# 创建assign_op操作
 assign_op1 = w.assign(10)
 
-#对w进行初始化
+# 对w进行初始化
 sess.run(w.initializer)
-#输出w
-print(sess.run(w)) # > 3
-#进行assign_op操作后输出
-print(sess.run(assign_op1)) # >10
+# 输出w
+print(sess.run(w))  # > 3
+# 进行assign_op操作后输出
+print(sess.run(assign_op1))  # >10
 
 ###############test 2###############
-#定义操作assign_op2
-assign_op2 = w.assign(2*w)
+# 定义操作assign_op2
+assign_op2 = w.assign(2 * w)
 
-#重新进行全局初始化，此时w=3
+# 重新进行全局初始化，此时w=3
 sess.run(tf.global_variables_initializer())
 
-print(sess.run(assign_op2)) # >> 6
-print(sess.run(assign_op2)) # >> 12
-print(sess.run(assign_op2)) # >> 24
+print(sess.run(assign_op2))  # >> 6
+print(sess.run(assign_op2))  # >> 12
+print(sess.run(assign_op2))  # >> 24
 
 sess.close()
 
 #############test 3#############
-#不同的session不会相互干扰
-#定义两个Session
-sess1=tf.Session()
-sess2=tf.Session()
+# 不同的session不会相互干扰
+# 定义两个Session
+sess1 = tf.Session()
+sess2 = tf.Session()
 
-#两个session分别对w进行初始化
+# 两个session分别对w进行初始化
 sess1.run(w.initializer)
 sess2.run(w.initializer)
 
